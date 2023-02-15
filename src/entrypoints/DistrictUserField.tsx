12733ee26b527bec31759d50ba1191193319df27
Bebe Jane <bjornthief@gmail.com>
2023-02-15 12:31:17 +0100

    fix

--- src/entrypoints/DistrictUserField.tsx
+++ src/entrypoints/DistrictUserField.tsx
@@ -1,7 +1,6 @@
 import { RenderFieldExtensionCtx } from 'datocms-plugin-sdk';
 import { Canvas, SelectField } from 'datocms-react-ui';
 import { useEffect, useState } from 'react';
-import { buildClient } from '@datocms/cma-client-browser';
 
 export type PropTypes = {
   ctx: RenderFieldExtensionCtx;
@@ -21,24 +20,14 @@ export default function DistrictUserField({ ctx }: PropTypes) {
 
   useEffect(() => {
 
-    const users = ctx.users
-    console.log(ctx)
-    console.log(ctx.users)
-    /*
-    const options = users.map(({ id, name, email }) => ({ value: id as string, label: name as string, email: email as string }))
+    const currentValue = ctx.formValues[ctx.field.attributes.api_key];
+    const options: DistrictUserOption[] = []
+    Object.keys(ctx.users).forEach(k => options.push({ label: ctx.users[k]?.attributes.email as string, value: k }))
 
     setOptions(options)
 
     if (currentValue)
       setValue(options.find(({ value }) => value === currentValue))
-    else {
-      //@ts-ignore
-      const userEmail = ctx.currentUser.attributes.email
-      setValue(options.find(({ email }) => email.toLowerCase() === userEmail))
-    }
-    */
-
-
   }, [setOptions])
 
   useEffect(() => {
